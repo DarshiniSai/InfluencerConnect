7bee8de87d261d23a40495ab0a2293471b1a37d6
Darshini Sai Mathi <159278720+DarshiniSai@users.noreply.github.com>
2025-08-02 21:32:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,21 @@ const userRoutes = require('./routes/users');
 const campaignsRoutes = require('./routes/campaigns');
 
 const app = express();
+
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'https://influencerconnect.onrender.com'],
+  origin: ['http://127.0.0.1:5500', 'https://influencerconnect.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
-app.use(express.json());
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
 app.use(bodyParser.json());
+
 app.use('/users', userRoutes);
 app.use('/campaigns', campaignsRoutes);
 
-
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
