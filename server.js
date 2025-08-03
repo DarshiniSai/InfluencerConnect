cfc3a0226706394799e5cfb8947c39ceddfb02be
Darshini Sai Mathi <159278720+DarshiniSai@users.noreply.github.com>
2025-08-03 12:14:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,23 +7,10 @@ const campaignsRoutes = require('./routes/campaigns');
 const app = express();
 
 app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = [
-      'http://127.0.0.1:5500',
-      'https://influencerconnect.vercel.app',
-      'https://influencer-connect-frontend.vercel.app',
-      'https://influencer-connect-frontend.onrender.com',
-      'https://influencer-connect-frontend-4yhn5ztzc.vercel.app'
-    ];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
 }));
+
 
 app.use(express.static('public'));
 app.use(express.json({ limit: '10mb' }));
